2be6307d6a51fc658fbb8babb12e5edd62eeb3ac
Hocnv0204 <nguyenhh1102@gmail.com>
2024-04-03 15:36:32 +0700

    init project

--- HelloWorld.asm
+++ HelloWorld.asm
@@ -1,15 +1,16 @@
+;Viet ra man hinh Hello World
 .model small         
 .stack 100h
 .data
 mess db 'Hello World!$'
 .code
 main proc
-    mov ax,@data 
-    mov ds,ax
+    mov ax,@data  ;nap dia chi doan du lieu vao ax
+    mov ds,ax     ;ds chua dia chi doan du lieu
     lea dx,mess
-    mov ah,9h
-    int 21h 
-    mov ax,4ch
+    mov ah,9h     ;ham 9h hien thi xau ki tu
+    int 21h       ;goi ham 21h
+    mov ax,4ch    ;ham 4ch tro ve dos
     int 21h
 main endp
 end main
